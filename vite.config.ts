e1e8b35138f29156679ea04e9b8ef748c6306a62
mhfed <nmhieu04091999@gmail.com>
2024-11-19 10:01:54 +0700

    refactor: Update path aliases in tsconfig.json and vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -7,11 +7,16 @@ export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
       '@components': path.resolve(__dirname, './src/components'),
       '@pages': path.resolve(__dirname, './src/pages'),
       '@assets': path.resolve(__dirname, './src/assets'),
       '@utils': path.resolve(__dirname, './src/utils'),
+      '@hooks': path.resolve(__dirname, './src/hooks'),
+      '@constants': path.resolve(__dirname, './src/constants'),
+      '@types': path.resolve(__dirname, './src/types'),
+      '@services': path.resolve(__dirname, './src/services'),
+      '@styles': path.resolve(__dirname, './src/styles'),
+      '@': path.resolve(__dirname, './src'),
     },
   },
   // server: {
